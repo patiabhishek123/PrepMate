4028bbae1c6226fd1063926ea182fc90c0e26126
patiabhishek123 <abhishekcloud2003@gmail.com>
2025-08-15 23:08:08 +0530

    Home page UI updated

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import dayjs from 'dayjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { getRandomInterviewCover } from '@/lib/utils';
+import DisplayTechIcons from './DisplayTechIcons';
 
 const Interviewcard = ({interviewId,userId,role,type,techstack,createdAt}:InterviewCardProps) => {
 
@@ -7,7 +12,41 @@ const Interviewcard = ({interviewId,userId,role,type,techstack,createdAt}:Interv
   const normalizedType=/mix/gi.test(type)? 'Mixed' :type;
   const formattedDate=dayjs(feedback?.createdAt||createdAt||Date.now()).format('MMM D, YYYY')
   return (
-    <div className='card-border w-[3605px] max-sm:w-full min-h-96'></div>
+    <div className='card-border w-[360px] max-sm:w-full min-h-96'>
+      <div className='card-interview'>
+        <div className='absolute top-0 right-0 w-fir px-4 py-2 rounded-bl-lg bg-light-600'>
+          <p className='badge-text'>{normalizedType}</p>
+        </div>
+        <Image src={getRandomInterviewCover()} alt="cover img" width={90} height={90} className='rounded-full object-fit size-[90px]'></Image>
+        <h3 className='mt-5 capitalize'>
+          {role} Intereview
+        </h3>
+        <div className='flex flex-row gap-5 mt-3'>
+          <div className='flex flex-row gap-2'>
+            <Image src="/calendar.svg" alt='calender' width={22} height={22}></Image>
+            <p>{formattedDate}</p>
+          </div>
+          <div className='flex flex-row gap-2 items-center'
+          >
+            <Image src="/star.svg" alt='star' width={22} height={22}/>
+            <p>
+              {feedback?.totalScore||'---'}/100</p>
+          </div>
+        </div>
+        <p className='line-clamp-2 mt-5'>
+          {feedback?.finalAssessment||"you haven't taken an Interview yet.take it now to improve you skills"}
+        </p>
+         <div className='flex flex-row justify-between'>
+        
+        <DisplayTechIcons techStack={techstack}/>
+        <Button className='btn-primary'>
+          <Link href={feedback?`/interview/${interviewId}/feedback`:`/interview/${interviewId}`}>
+          {feedback?'Check Feedback':'View assesment'}</Link>
+        </Button>
+      </div>
+      </div>
+     
+    </div>
   )
 }
 
