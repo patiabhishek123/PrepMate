419872d64070c29b8144d35a3ee68f59aaa611b5
patiabhishek123 <abhishekcloud2003@gmail.com>
2025-08-31 19:49:36 +0530

    interview generation UI implemented

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h3>Integrview generation</h3>
+      <Agent userName="You" type="generate"></Agent>
+    </>
+  )
+}
+
+export default page
